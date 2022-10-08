76fe86e63587e44ceb6d498c4d0f481e99450518
timlednev <timlednev@mail.ru>
2022-10-08 14:09:05 +0300

    Add 365 file

--- klerk_parser.py
+++ klerk_parser.py
@@ -15,30 +15,43 @@ def parse_klerk_news(days=1):
     while (True):
         response = rq.get(KLERK_NEWS_URL + str(page_number))
         page_number += 1
+        error_count = 0
         if (response.status_code == 200):
             news_urls = re.findall('klerk\.ru/buh/news/[\d]*/\"', response.text)[:-5]
             for news_url in news_urls:
-                res = {}
-                url = 'https://www.' + news_url[:-1]
-                res['url']= url
-                news = rq.get(url).text
-                soup = bs(news, 'html.parser')
-                res['site'] = 'klerk'
-                res['title'] = soup.body.find("h1").text.replace("\xa0", " ").replace("\t", " ")
-                res['description'] = soup.body.find(class_="article__resume").text.replace("\xa0", " ").replace("\t", " ")
-                res['text'] = soup.body.find(class_="article__content").text.replace("\xa0", " ").replace("\t", " ")
-                date_raw = re.findall('\d{4}-\d+-\d+ \d+:\d+:\d+', str(soup.body.find(class_="status__block")))[0]
-                date, time = date_raw.split()
-                date = date.split('-')
-                time = time.split(':')
-                res['timestamp'] = dt.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2])).timestamp()
-                if (days * 86400 + res['timestamp'] < dt.datetime.now().timestamp()):
-                    return ress
-                ress.append(res)
+                try:
+                    res = {}
+                    url = 'https://www.' + news_url[:-1]
+                    res['url']= url
+                    news = rq.get(url).text
+                    soup = bs(news, 'html.parser')
+                    res['site'] = 'klerk'
+                    res['title'] = soup.body.find("h1").text.replace("\xa0", " ").replace("\t", " ")
+                    res['text'] = soup.body.find(class_="article__content").text.replace("\xa0", " ").replace("\t", " ")
+                    try:
+                        res['description'] = soup.body.find(class_="article__resume").text.replace("\xa0", " ").replace("\t", " ")
+                    except:
+                        if (len(res['text']) > 100):
+                            res['description'] = res['text'][:100] + '...'
+                        else:
+                            res['description'] = res['text']
+                    date_raw = re.findall('\d{4}-\d+-\d+ \d+:\d+:\d+', str(soup.body.find(class_="status__block")))[0]
+                    date, time = date_raw.split()
+                    date = date.split('-')
+                    time = time.split(':')
+                    res['timestamp'] = dt.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2])).timestamp()
+                    if (days * 86400 + res['timestamp'] < dt.datetime.now().timestamp()):
+                        return ress
+                    ress.append(res)
+                except Exception as e:
+                    error_count += 1
+                    print(e, url)
+                    if (error_count > 0):
+                        return ress
             
             
 
 if __name__ == "__main__":
-    res = parse_klerk_news(30)
-    with open('klerk_news.json', 'w') as outfile:
+    res = parse_klerk_news(365)
+    with open('klerk_news_365.json', 'w') as outfile:
         json.dump(res, outfile)
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 pip3 install beautifulsoup4 pandas numpy IPython
 
-python3 ./cfo_parser.py
+# python3 ./cfo_parser.py
 python3 ./klerk_parser.py
-python3 ./consultant_parser.py
+# python3 ./consultant_parser.py
